7cdd1509d34538f4b2b00c92871a3e432bceeb95
paulosilva159 <paulo.victor158@gmail.com>
2021-02-23 02:58:05 -0300

    fixing one import

--- flutterando_pokedex_challenge/lib/presentation/pokedex/outside_pokedex.dart
+++ flutterando_pokedex_challenge/lib/presentation/pokedex/outside_pokedex.dart
@@ -3,8 +3,7 @@ import 'package:flutterando_pokedex_challenge/presentation/common/outside_backgr
 import 'package:flutterando_pokedex_challenge/presentation/common/painters/right_roller_painter.dart';
 import 'package:flutterando_pokedex_challenge/presentation/common/utils/colors.dart';
 import 'package:flutterando_pokedex_challenge/presentation/common/utils/proportions.dart';
-
-import 'file:///D:/Projetos/Flutter/flutterando_challenges/flutterando_pokedex_challenge/lib/presentation/pokedex/inner_cover.dart';
+import 'package:flutterando_pokedex_challenge/presentation/pokedex/inner_cover.dart';
 
 class OutsidePokedex extends StatelessWidget {
   @override
